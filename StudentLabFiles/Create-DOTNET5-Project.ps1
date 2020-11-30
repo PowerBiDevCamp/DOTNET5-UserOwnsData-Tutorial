a61c3f83646014855c887f074a72fe22f4051441
Ted Pattison <ted@tedpattison.net>
2020-11-30 11:01:20 -0500

    Updates

--- StudentLabFiles/Create-DOTNET5-Project.ps1
+++
@@ -1,8 +0,0 @@
-dotnet new mvc --auth SingleOrg --framework netcoreapp3.1
-
-dotnet remove package Microsoft.AspNetCore.Authentication.AzureAD.UI
-
-# update to latest available version of Microsoft.Identity.Web
-dotnet add package Microsoft.Identity.Web -v 0.3.0-preview
-dotnet add package Microsoft.Identity.Web.UI -v 0.3.0-preview
-dotnet add package Microsoft.PowerBi.Api
